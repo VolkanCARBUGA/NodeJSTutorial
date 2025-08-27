aacc8a6b86ced1202ec597155312d39fc9bf3ba4
VolkanCARBUGA <volkanilkyar@gmail.com>
2025-08-27 17:43:22 +0300

    remove node modules

--- 9.nodejs-socket/server.js
+++ 9.nodejs-socket/server.js
@@ -7,10 +7,36 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
+app.use(express.static("public"));
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const users=new Set();
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+ 
+  socket.on("join", (userName) => {
+    users.add(userName);
+    socket.userName = userName;
+    io.emit("user-joined", userName);
+    io.emit("user-list", Array.from(users));
+  });
+  socket.on("chat-message", (message) => {
+   io.emit("chat-message", message);
+  });
+  socket.on("disconnect", () => {
+    console.log("a user disconnected", socket.id);
+    users.forEach((user) => {
+      if (user.id === socket.userName) {
+        users.delete(user);
+        io.emit("user-left", user);
+        io.emit("user-list", Array.from(users));
+      }
+    });
+    
+  });
+  
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
